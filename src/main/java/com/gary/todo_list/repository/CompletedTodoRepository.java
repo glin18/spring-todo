0adffbfc69e3da33a3a0be8840b51dd824a6c3ca
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-01-13 14:46:44 +0800

    Add repository annotation

--- src/main/java/com/gary/todo_list/repository/CompletedTodoRepository.java
+++ src/main/java/com/gary/todo_list/repository/CompletedTodoRepository.java
@@ -1,9 +1,11 @@
 package com.gary.todo_list.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.gary.todo_list.entity.CompletedTodo;
 
+@Repository
 public interface CompletedTodoRepository extends JpaRepository<CompletedTodo, Long> {
 
 }
